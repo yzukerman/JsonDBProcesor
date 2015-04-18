48744bc855f04997bc9a8e9fb896dffcd0093a18
Yuval Zukerman <yuval@enavigo.com>
2015-04-18 13:18:08 -0400

    Initial add

---
+++ JsonDBProcessor/src/com/enavigo/doleloader/DBLoader.java
@@ -0,0 +1,81 @@
+package com.enavigo.doleloader;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.enavigo.doleloader.pojo.Product;
+
+public class DBLoader {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Guess who's trying to be back?...");
+		
+		Product recipe = new Product();
+		ObjectMapper mapper = new ObjectMapper();
+		Connection connection = null;
+		List<Product> recipes = new ArrayList<Product>();
+		
+		try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/doledb?" +
+				                                   "user=root&password=ima711");
+			System.out.println("DB Connection established...");
+			
+			JsonNode tree = mapper.readTree(new File("/Users/yuvalzukerman/Development/dole-db-loader/dole-salads-ca-products.json"));
+			System.out.println(tree.size());
+			Iterator<String> fieldNames = tree.fieldNames();
+			while (fieldNames.hasNext())
+			{
+				System.out.println(fieldNames.next());
+			}
+			Iterator<JsonNode> iterator = tree.elements();
+			while(iterator.hasNext())
+			{
+				JsonNode currentNode = iterator.next();
+				System.out.println("Node size: " + currentNode.size());
+				Iterator<JsonNode> productIterator = currentNode.elements();
+				while(productIterator.hasNext())
+				{
+					JsonNode productNode = productIterator.next();
+					System.out.println(productNode.get("title"));
+				}
+			}
+			
+			
+        } catch (Exception ex) {
+            // handle the error
+        	System.out.println("Error: " + ex.getMessage());
+        }
+		finally
+		{
+			try
+			{
+				if (connection != null)
+				{
+					connection.close();
+					System.out.println("DB Connection closed");
+				}
+			}
+			catch(Exception e)
+			{
+				System.out.println("Issue closing DB connection: " + e.getMessage());
+			}
+				
+		}
+	}
+
+}
