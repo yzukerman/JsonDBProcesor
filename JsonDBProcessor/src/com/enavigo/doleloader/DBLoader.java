88b65c509dfc10cf9a42c9fdf8e9746a53cd7f1b
Yuval Zukerman <yuval@enavigo.com>
2015-04-19 12:27:35 -0400

    Refactored to use a JSON file (dole-config.json) for configuration information as to what file to read as input and what Mapper class to use to read the file.

--- JsonDBProcessor/src/com/enavigo/doleloader/DBLoader.java
+++ JsonDBProcessor/src/com/enavigo/doleloader/DBLoader.java
@@ -1,16 +1,20 @@
 package com.enavigo.doleloader;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -22,13 +26,26 @@ public class DBLoader {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Guess who's trying to be back?...");
+			
 		
-		Product recipe = new Product();
 		ObjectMapper mapper = new ObjectMapper();
-		Connection connection = null;
-		List<Product> recipes = new ArrayList<Product>();
+		Connection connection = null;;
+		List<HashMap<String, String>> tasks = null;
+		
+		// load tasks from config file
+		try
+		{
+			tasks = loadTasks();
+		}
+		catch (JsonProcessingException jpe)
+		{
+			System.out.println("Failed to open config file." + jpe.getLocalizedMessage());
+		}
+		catch (IOException ioe)
+		{
+			System.out.println("Failed to open config file." + ioe.getMessage());
+		}
+		
 		
 		try {
             // The newInstance() call is a work around for some
@@ -37,40 +54,21 @@ public class DBLoader {
 				                                   "user=root&password=ima711");
 			System.out.println("DB Connection established...");
 			
-			JsonNode tree = mapper.readTree(new File("/Users/yuvalzukerman/Dropbox/Enavigo/Clients/Dole/Extracts/dolesalads.ca/dole-salads-ca-products.json"));
-//			Class c = Class.forName("com.enavigo.doleloader.mapper.DoleSaladsCaJsonMapper");
-//			JsonMapper jsonMapper = new DoleSaladsCaJsonMapper();
-//			jsonMapper = (JsonMapper) c.cast(jsonMapper);
-			
-			
-
-			JsonMapper jsonMapper;
-			jsonMapper =
-			  (JsonMapper) Class.forName("com.enavigo.doleloader.mapper.DoleSaladsCaProductMapper").newInstance();
-			jsonMapper.mapJson(tree);
-//			System.out.println(tree.size());
-//			Iterator<String> fieldNames = tree.fieldNames();
-//			while (fieldNames.hasNext())
-//			{
-//				System.out.println(fieldNames.next());
-//			}
-//			Iterator<JsonNode> iterator = tree.elements();
-//			while(iterator.hasNext())
-//			{
-//				JsonNode currentNode = iterator.next();
-//				System.out.println("Node size: " + currentNode.size());
-//				Iterator<JsonNode> productIterator = currentNode.elements();
-//				while(productIterator.hasNext())
-//				{
-//					JsonNode productNode = productIterator.next();
-//					System.out.println(productNode.get("title"));
-//				}
-//			}
+			for(HashMap<String, String> task : tasks)
+			{
+				JsonNode tree = mapper.readTree(new File(task.get("source-file")));
+				JsonMapper jsonMapper;
+				jsonMapper =
+				  (JsonMapper) Class.forName(task.get("mapper")).newInstance();
+				System.out.println("Step: " + task.get("name"));
+				jsonMapper.mapJson(tree);
+			}
 			
 			
         } catch (Exception ex) {
             // handle the error
         	System.out.println("Error: " + ex.getMessage());
+        	ex.printStackTrace();
         }
 		finally
 		{
@@ -88,6 +86,33 @@ public class DBLoader {
 			}
 				
 		}
+	}
+	
+	private static List<HashMap<String, String>> loadTasks() throws JsonProcessingException, IOException
+	{
+		List<HashMap<String, String>> tasks = new ArrayList<HashMap<String, String>>();
+		ObjectMapper mapper = new ObjectMapper();
+		
+		
+			Path currentRelativePath = Paths.get("");
+			String s = currentRelativePath.toAbsolutePath().toString();
+			System.out.println("Current relative path is: " + s);
+			JsonNode taskTree = mapper.readTree(new File("./config/dole-config.json"));
+			
+			JsonNode taskList = taskTree.get("tasks");
+			Iterator<JsonNode> taskIterator = taskList.elements();
+			while(taskIterator.hasNext())
+			{
+				JsonNode taskNode = taskIterator.next();
+				System.out.println(taskNode.get("name"));
+				HashMap<String, String> task = new HashMap<String, String>();
+				task.put("name", taskNode.get("name").asText());
+				task.put("source-file", taskNode.get("source-file").asText());
+				task.put("mapper", taskNode.get("mapper").asText());
+				tasks.add(task);
+			}
+			
+			return tasks;
 	}
 
 }
