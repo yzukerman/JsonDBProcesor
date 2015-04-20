6f9b4decbd8559935747870bb67ecf6cfa0be67b
Yuval Zukerman <yuval@enavigo.com>
2015-04-20 12:17:16 -0400

    Corrected issue with finding the next available identifier.

--- JsonDBProcessor/src/com/enavigo/doleloader/persistence/DoleProductPersistor.java
+++ JsonDBProcessor/src/com/enavigo/doleloader/persistence/DoleProductPersistor.java
@@ -32,9 +32,10 @@ public class DoleProductPersistor implements DoleJsonPersistor {
 		this.connection = connection;
 		// TODO Auto-generated method stub
 		List<Product> products = (List<Product>)objToPersist;
-		int nextProductId = getMaxId(connection, "PRODUCT") + 1;
-		int nextRelatedRecipeId = getMaxId(connection, "product_related_recipe") + 1;
-		
+		int nextProductId = getMaxId(connection, "PRODUCT", "product_id") + 1;
+		int nextRelatedRecipeId = getMaxId(connection, "product_related_recipe", 
+				"product_related_recipe_id") + 1;
+		System.out.println ("nextProductId = " + nextProductId + ", nextRelatedRecipeId = " + nextRelatedRecipeId);
 		for(Product p : products)
 		{
 			persistProduct(connection, p, nextProductId, sourceSite);
@@ -51,20 +52,20 @@ public class DoleProductPersistor implements DoleJsonPersistor {
 	 * Returns the highest numbered ID in the products table
 	 * @param connection
 	 * @param tableName - the table we want to get the maximum ID for
+	 * @param idColumn - the column name containing the table's primary key
 	 * @return the highest id used in the product table
 	 * @throws SQLException
 	 */
-	private int getMaxId(Connection connection, String tableName) throws SQLException
+	private int getMaxId(Connection connection, String tableName, String idColumn) throws SQLException
 	{
-		String queryString = DoleLoaderConstants.PRODUCT_PERSIST_GET_MAX_ID + tableName;
+		String queryString = "SELECT MAX(" + idColumn + ") AS max_id FROM " + tableName;
 		PreparedStatement query = connection.prepareStatement(queryString);
-		
 		ResultSet rs = query.executeQuery();
 		System.out.println("Resultset size " + rs.getFetchSize());
 		int maxProductId = 0;
 		while(rs.next())
 		{
-			maxProductId = rs.getInt("max_product_id");
+			maxProductId = rs.getInt("max_id");
 		}
 		
 		return maxProductId;
@@ -94,7 +95,6 @@ public class DoleProductPersistor implements DoleJsonPersistor {
 		query.setString(12, p.getNutritionlabelHtml());
 		
 		int result = query.executeUpdate();
-		System.out.println("Product Insert Result = " + result);
 	}
 	
 	/**
@@ -124,7 +124,6 @@ public class DoleProductPersistor implements DoleJsonPersistor {
 			
 			
 			int result = query.executeUpdate();
-			System.out.println("Product Related Recipe Insert Result = " + result);
 			recipeId++;
 		}
 		
