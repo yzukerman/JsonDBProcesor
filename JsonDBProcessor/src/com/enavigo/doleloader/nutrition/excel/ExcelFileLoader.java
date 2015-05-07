1430ee92b0b7ec37e5ae961ec6726e6f95971927
Yuval Zukerman <yuval@enavigo.com>
2015-05-07 10:43:21 -0400

    Successfully reading nutrients from Excel spreadsheets

--- JsonDBProcessor/src/com/enavigo/doleloader/nutrition/excel/ExcelFileLoader.java
+++ JsonDBProcessor/src/com/enavigo/doleloader/nutrition/excel/ExcelFileLoader.java
@@ -65,14 +65,127 @@ public class ExcelFileLoader {
 		
 		System.out.println(cell.getStringCellValue());
 		topNutrients.put("recipeTitle", cell.getStringCellValue());
+		
 		// serving size
 		cell = sheet.getRow(3).getCell(0);
-		//String value -
+		String value = cell.getStringCellValue().replaceAll("\\D","");
+		topNutrients.put("serving_size", value + "g");
+
+		// calories
+		cell = sheet.getRow(7).getCell(1);
+		double dvalue = cell.getNumericCellValue();
+		topNutrients.put("calories", (int)dvalue);
+		
+		// calories from fat
+		cell = sheet.getRow(8).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("calories_from_fat", (int)dvalue);
+		
+		// total fat
+		cell = sheet.getRow(10).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("total_fat", (int)dvalue);
+
+		// total fat percentage
+		cell = sheet.getRow(10).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("total_fat_percentage", (int)dvalue);
+
+		// saturated fat
+		cell = sheet.getRow(11).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("saturated_fat", (int)dvalue);
+
+		cell = sheet.getRow(11).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("saturated_fat_percentage", (int)dvalue);
+		
+		// trans fat
+		cell = sheet.getRow(12).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("trans_fat", (int)dvalue);
+
+		// polyunsaturated fat DOUBLE
+		cell = sheet.getRow(13).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("poly_fat", dvalue);
+
+		// mono fat
+		cell = sheet.getRow(14).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("mono_fat", (int)dvalue);
+
+		// cholesterol
+		cell = sheet.getRow(15).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("cholesterol", (int)dvalue);
+
+		cell = sheet.getRow(15).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("cholesterol_percentage", (int)dvalue);
+
+		// sodium
+		cell = sheet.getRow(16).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("sodium", (int)dvalue);
+
+		cell = sheet.getRow(16).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("sodium_percentage", (int)dvalue);
+		
+		// potassium
+		cell = sheet.getRow(17).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("potassium", (int)dvalue);
+
+		cell = sheet.getRow(17).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("potassium_percentage", (int)dvalue);
+		
+		// carbs
+		cell = sheet.getRow(18).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("carbs", (int)dvalue);
+
+		cell = sheet.getRow(18).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("carbs_percentage", (int)dvalue);
+
+		// fiber
+		cell = sheet.getRow(19).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("fiber", (int)dvalue);
+
+		cell = sheet.getRow(19).getCell(3);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("fiber_percentage", (int)dvalue);
+
+		// sugars
+		cell = sheet.getRow(20).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("sugars", (int)dvalue);
+
+		// protein
+		cell = sheet.getRow(21).getCell(1);
+		dvalue = cell.getNumericCellValue();
+		topNutrients.put("protein", (int)dvalue);
+
+		String nutName = null;
+		System.out.println(sheet.getRow(23).getCell(0).getCellType());
+		// variable nutrients
+		for(int x = 23; sheet.getRow(x) != null; x++)
+		{
+			System.out.println("X: " + x);
+			nutName = sheet.getRow(x).getCell(0).getStringCellValue();
+			dvalue = sheet.getRow(x).getCell(3).getNumericCellValue();
+			bottomNutrients.put(nutName, (int)dvalue);
+		}
 		
 		nutrients.add(topNutrients);
 		nutrients.add(bottomNutrients);
 		return nutrients;
 	}
+	
 	
 	private void persistNutrients(List<HashMap<String, Object>> nutrients, Connection connection)
 	{
@@ -88,6 +201,7 @@ public class ExcelFileLoader {
 			List<HashMap<String, Object>> nutrients = 
 					efl.processFile("/Users/yuvalzukerman/Dropbox/Enavigo/Clients/Dole/Nutrition/nutritionLabels/Baker Beach Salad.xlsx");
 			System.out.println(nutrients.get(0));
+			System.out.println(nutrients.get(1));
 		}
 		catch(InvalidFormatException ife)
 		{
